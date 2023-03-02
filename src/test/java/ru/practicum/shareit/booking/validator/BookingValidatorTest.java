1ef2873c463e98c8444eb4b2b09c493bf19802a2
brish <brishtenk@yandex.ru>
2023-03-02 21:06:51 +0300

    fix: checkstyle

--- src/test/java/ru/practicum/shareit/booking/validator/BookingValidatorTest.java
+++ src/test/java/ru/practicum/shareit/booking/validator/BookingValidatorTest.java
@@ -13,9 +13,9 @@ public class BookingValidatorTest {
         BookingValidator validationService = new BookingValidator();
         LocalDateTime dateTime1 = LocalDateTime.of(2023, 2, 5, 11, 11);
         LocalDateTime dateTime2 = LocalDateTime.of(2023, 2, 6, 11, 11);
-        BookingDtoRequest dto1 = new BookingDtoRequest( 1L, dateTime2, dateTime1);
+        BookingDtoRequest dto1 = new BookingDtoRequest(1L, dateTime2, dateTime1);
         Assertions.assertThrows(ValidationException.class, () -> validationService.isValidBooking(dto1));
-        BookingDtoRequest dto2 = new BookingDtoRequest( 1L, dateTime1, dateTime2);
+        BookingDtoRequest dto2 = new BookingDtoRequest(1L, dateTime1, dateTime2);
         Assertions.assertDoesNotThrow(() -> validationService.isValidBooking(dto2));
     }
 }
