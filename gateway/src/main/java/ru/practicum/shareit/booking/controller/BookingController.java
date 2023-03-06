8a511eb56ba85e69cd67dd252cdb775b3be8ee36
brish <brishtenk@yandex.ru>
2023-03-06 03:32:05 +0300

    fix: postman tests

--- gateway/src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -4,13 +4,14 @@ package ru.practicum.shareit.booking.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.booking.client.BookingClient;
 import ru.practicum.shareit.booking.dto.BookingDtoRequest;
 import ru.practicum.shareit.booking.model.BookingState;
 import ru.practicum.shareit.exceptions.UnknownStateException;
+import ru.practicum.shareit.utils.Create;
 
-import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
 
@@ -27,7 +28,7 @@ public class BookingController {
                                                     @PositiveOrZero @RequestParam(name = "from", defaultValue = "0") Integer from,
                                                     @Positive @RequestParam(name = "size", defaultValue = "10") Integer size) {
         BookingState state = BookingState.from(stateParam)
-                .orElseThrow(() -> new IllegalArgumentException("Unknown state: " + stateParam));
+                .orElseThrow(() -> new UnknownStateException("Unknown state: " + stateParam));
         log.info("Get booking with state {}, userId={}, from={}, size={}", stateParam, userId, from, size);
         return bookingClient.getBookingsOfUser(userId, state, from, size);
     }
@@ -41,7 +42,7 @@ public class BookingController {
 
     @PostMapping
     public ResponseEntity<Object> createBooking(@RequestHeader("X-Sharer-User-Id") long userId,
-                                                @RequestBody @Valid BookingDtoRequest requestDto) {
+                                                @RequestBody @Validated({Create.class}) BookingDtoRequest requestDto) {
         log.info("Creating booking {}, userId={}", requestDto, userId);
         return bookingClient.createBooking(userId, requestDto);
     }
